717f35ec1b3581bcefd330567ac0ebb9ffe1cd6a
Nicola Baribeau <nicolabaribeau@gmail.com>
2025-08-24 01:48:38 -0400

    This shouldn't be here

--- LockList/Lock.gd
+++ LockList/Lock.gd
@@ -2,6 +2,7 @@ class_name Lock
 var name: String
 
 static var MouseOverLock: Lock = Lock.new("MouseOver")
+static var PerceptionLock: Lock = Lock.new("Perception")
 
 func _init(_name: String = "") -> void:
 	name = _name
